01b065567744497d45f7756dfefa7332b45f7981
Marvin Lutz <marvin.lutz@fh-bielefeld.de>
2016-10-06 15:56:25 +0200

    [BROKEN] Work in Progress!!! - Added submenu functions - refactoring for REDBrick integration

--- angular/factory/extendedView/red/brickREDExtended.js
+++ angular/factory/extendedView/red/brickREDExtended.js
@@ -12,13 +12,32 @@ angApp.factory('BrickREDExtended', ['$log', 'TF', function($log, TF){
     BrickREDExtended.prototype.getController = function(){
         return ['$scope', '$log', 'TF', function($scope, $log, TF){
             let debug_name = "[BrickREDExtended|" + $scope.uid + "]";
-            $scope.viewName = "BrickREDExtended"; //just for test purpose
+
 
             $log.log(debug_name + " is here!");
             $log.log($scope.deviceInfo);
 
             $scope.device = TF.getDeviceImpl($scope.deviceInfo.deviceClassName, $scope.uid);
             $log.log($scope.device);
+
+
+            //Add Submenus to navigation
+            // let submenuConfig = [
+            //     {
+            //         name: "Overview",
+            //         target: $scope.uid
+            //     }, {
+            //         name: "Program",
+            //         target: $scope.uid+"-redbrick-program"
+            //     }, {//TODO delete debug entries
+            //
+            //         target: "missing Name"
+            //     }, {
+            //         name: "missing target"
+            //     }
+            // ]
+            // $scope.addNavigationSubmenu($scope.uid, submenuConfig);
+
         }];
     }
 
